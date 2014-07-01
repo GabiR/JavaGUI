145d67ab5bc3d5f096b034ed5daa9c0067d434ca
Student User <student@cs.pub.ro>
2014-07-01 15:35:56 +0300

    second gui

---
+++ SecondGUI.java
@@ -0,0 +1,14 @@
+import javax.swing.JOptionPane;
+
+
+public class SecondGUI {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null, "bla 1", "bla bla 2", 0);
+	}
+
+}
